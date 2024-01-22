455f8e62272891d9a2ca8b2c59fc5157d3a8a62e
Zio <143772055+ZioSenkai@users.noreply.github.com>
2024-01-22 22:05:06 +0700

    fix how to toggle active the blockquote

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,10 @@
 'use strict'
 
-const btn = document.querySelectorAll('.btn');
+const accordion = document.querySelectorAll('.contentBx');
 
-btn.forEach(btn => {
-  btn.addEventListener('click', function () {
-    this.classList.toggle('active');
-    const blockquoteContent = this.nextElementSibling;
-    const plusIcon = this.querySelector('.plus-icon');
-    const minusIcon = this.querySelector('.minus-icon')
-
-    if (blockquoteContent.style.maxHeight) {
-      blockquoteContent.style.maxHeight = null
-      plusIcon.style.display = 'block'
-      minusIcon.style.display = 'none'
-    } else {
-      blockquoteContent.style.maxHeight = blockquoteContent.style.maxHeight + 'px'
-      plusIcon.style.display = 'none'
-      minusIcon.style.display = 'block'
-    }
+accordion.forEach(li => {
+  li.addEventListener('click', function () {
+    this.classList.toggle('active')
   })
 })
-
-
 
